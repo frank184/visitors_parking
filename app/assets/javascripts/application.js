aa0ede23f348125c87cb90ec07a8871551f2cec8
frank184 <francoisbelanger1993@hotmail.com>
2015-07-23 16:25:55 -0400

    Fixed application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sass
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
