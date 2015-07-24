1b15907626b59c7ec9e6f2a097db812cc8498eae
frank184 <francoisbelanger1993@hotmail.com>
2015-07-23 22:03:18 -0400

    Fixed devise_for :users missing & Stylying

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
 
   root 'visitors_parking_pages#index'
-  
+
   get 'about'   => 'visitors_parking_pages#about'
   get 'contact' => 'visitors_parking_pages#contact'
   get 'help'    => 'visitors_parking_pages#help'
