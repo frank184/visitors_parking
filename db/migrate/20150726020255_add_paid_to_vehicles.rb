e40cf46c55dceeabe91c9cc26f5a0819c49c480e
frank184 <francoisbelanger1993@hotmail.com>
2015-07-25 22:05:07 -0400

    Added @vehicle.paid? migration

---
+++ db/migrate/20150726020255_add_paid_to_vehicles.rb
@@ -0,0 +1,5 @@
+class AddPaidToVehicles < ActiveRecord::Migration
+  def change
+    add_column :vehicles, :paid?, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724213552) do
+ActiveRecord::Schema.define(version: 20150726020255) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -45,8 +45,9 @@ ActiveRecord::Schema.define(version: 20150724213552) do
     t.string   "model",         limit: 255
     t.integer  "year",          limit: 4
     t.integer  "user_id",       limit: 4
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
+    t.boolean  "paid?",                     default: false
   end
 
   add_index "vehicles", ["user_id"], name: "index_vehicles_on_user_id", using: :btree
